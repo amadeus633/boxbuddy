e790f5b62cf6cceff65773a530f90d8649211c4b
amadeus633 <60037780+amadeus633@users.noreply.github.com>
2023-04-16 21:22:42 -0400

    Update app.py
    - import statements reformatted
    - added icon
    - added file-picker

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit,
-                             QPushButton, QTabWidget, QGridLayout)
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QTabWidget,
+    QGridLayout,
+)
 from PyQt5.QtCore import QThreadPool, QRunnable
+from PyQt5.QtGui import QIcon  # Add this import
 import subprocess
 import platform
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
 class ScriptRunner(QRunnable):
     def __init__(self, command):
@@ -13,10 +25,8 @@ class ScriptRunner(QRunnable):
         self.command = command
 
     def run(self):
-    if platform.system() == "Windows":
-        subprocess.Popen(["cmd.exe", "/c"] + self.command, creationflags=subprocess.CREATE_NEW_CONSOLE)
-    else:
-        subprocess.Popen(["qterminal", "-e"] + [" ".join(self.command)])
+        subprocess.Popen(["qterminal", "-e"] + [" ".join(self.command) + "; read -p 'Press [Enter] to close the terminal...'"])
+
 
 def run_script1_with_input():
     ip_address = ip_entry.text()
@@ -39,16 +49,26 @@ def run_script3_with_input():
     runner = ScriptRunner(["python", "scripts/dirsearch.py", ddomain])
     QThreadPool.globalInstance().start(runner)
 
-def run_vpn():
-    runner = ScriptRunner(["python", "scripts/vpn.py"])
+def run_vpn(file):
+    runner = ScriptRunner(["python", "scripts/vpn.py", f"{file}"])
     QThreadPool.globalInstance().start(runner)
 
 def load_stylesheet(file_path):
     with open(file_path, "r") as file:
         return file.read()
+        
+def file_select():
+    global filename
+    Tk().withdraw()
+    filename = askopenfilename()
+    file_label.setText(filename.split('/')[-1])
 
 app = QApplication(sys.argv)
 app.setStyleSheet(load_stylesheet("styles.css"))
+
+# Set the application icon
+icon = QIcon("img/200x200.png")
+app.setWindowIcon(icon)
 
 window = QWidget()
 window.setWindowTitle("BoxBuddy")
@@ -61,13 +81,23 @@ tab_widget = QTabWidget()
 layout.addWidget(tab_widget)
 
 # Tab 1: Setup commands
+
+filename = 'None selected'
+
 setup_tab = QWidget()
 tab_widget.addTab(setup_tab, "Setup")
 setup_layout = QVBoxLayout()
 setup_tab.setLayout(setup_layout)
 
+file_btn = QPushButton("Select file")
+file_btn.clicked.connect(file_select)
+setup_layout.addWidget(file_btn)
+
+file_label = QLabel(filename)
+setup_layout.addWidget(file_label)
+
 button3 = QPushButton("vpn")
-button3.clicked.connect(run_vpn)
+button3.clicked.connect(lambda:run_vpn(filename))
 setup_layout.addWidget(button3)
 
 # Add setup tab content here

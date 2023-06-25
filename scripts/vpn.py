e456e0ea50dbd075d879b002fd53269969081be4
Gand0rf <gand0rf@protonmail.com>
2023-06-25 18:51:31 -0400

    Switched subprocess call to Popen

--- scripts/vpn.py
+++ scripts/vpn.py
@@ -3,23 +3,25 @@ import subprocess, sys
 def main(vpnfile):
 
     vpnfile = vpnfile.replace(';','')
-    command = f"sudo openvpn {vpnfile}"
+    command = f"sudo openvpn {vpnfile} &"
 
     try:
-        process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        
         while True:
             output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
+            error = process.stderr.readline()
+            if output == '':
                 break
             if output:
                 print(output.strip())
-
         exit_code = process.poll()
         if exit_code != 0:
-            print("The command exited with code {exit_code}")
+            print(f"The command exited with code {exit_code}")
+            print(f"{error}")
 
     except Exception as e:
-        print("An error occurred while executing the command: {e}")
+        print(f"An error occurred while executing the command: {e}")
     
 
 if __name__ == "__main__":
